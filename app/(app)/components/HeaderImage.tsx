a02a53b3a84a8d677dd6eca7c0963364bb116445
Manu G <manusvathgurudath@gmail.com>
2024-08-07 22:21:21 -0700

    W

--- app/(app)/components/HeaderImage.tsx
+++ app/(app)/components/HeaderImage.tsx
@@ -2,21 +2,26 @@ export function HeaderImage({
   src,
   alt,
   height,
+  darken,
 }: Readonly<{
   src: string;
   alt: string;
   height?: string;
+  darken?: boolean;
 }>) {
   return (
-    <img
-      src={src}
-      alt={alt}
-      style={{
-        width: "100%",
-        height: height || "400px",
-        objectFit: "cover",
-        borderRadius: 0,
-      }}
-    />
+    <div style={{ background: "black" }}>
+      <img
+        src={src}
+        alt={alt}
+        style={{
+          width: "100%",
+          height: height || "400px",
+          objectFit: "cover",
+          borderRadius: 0,
+          opacity: darken ? 0.5 : 1,
+        }}
+      />
+    </div>
   );
 }
